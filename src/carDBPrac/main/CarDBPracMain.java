487cb90694ba2c1842246a5f09cef0555ed339bf
youminsu <alstnsms95@gmail.com>
2018-01-31 15:40:35 +0900

    내용 추가

--- src/carDBPrac/main/CarDBPracMain.java
+++ src/carDBPrac/main/CarDBPracMain.java
@@ -1,10 +1,32 @@
 package carDBPrac.main;
 
+import java.util.Scanner;
+
 public class CarDBPracMain {
-
 	public static void main(String[] args) {
-		System.out.println("hey");
-
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+		System.out.println("=================");
+		System.out.println("번호를 입력하세요");
+		System.out.println("1. 자동차 정보 보기");
+		System.out.println("2. 자동차 정보 입력");
+		System.out.println("3. 자동차 정보 수정");
+		System.out.println("4. 자동차 정보 삭제");
+		System.out.println("==================");
+		int inputNum = sc.nextInt();
+		if(inputNum == 1) { // 자동차 정보 조회
+		} else if(inputNum == 2) { // 자동차 정보 입력
+			break;
+		} else if(inputNum == 3) { // 자동차 정보 수정 
+			break;
+		} else if(inputNum == 4) { // 자동차 정보 삭제
+			break;
+		} else { // 다른 번호 입력했을때
+			System.out.println("다시 입력해 주세요.");
+		}
+		}
+		System.out.println("종료");
 	}
 
 }
